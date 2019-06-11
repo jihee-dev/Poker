2ea5e595468f8b311e9ce6a5fa9ad9811bbcc9e4
plat67 <good2060@naver.com>
2019-06-11 22:21:13 +0900

    Update Main

--- Poker/Main.cpp
+++ Poker/Main.cpp
@@ -4,48 +4,52 @@
 
 using namespace std;
 
-int main() {
-	playingGame();
-}
-
-void playingGame() {
-	GameController* controller = new GameController();
-	Dealer* dealer = new Dealer();
-	int playerNum = 3;
-	User user[3];
-
-	// 플레이어 수 입력
-	playerNum = controller->askPlayerNum();
-	cout << endl;
-
-	// 유저 생성 > 플레이어 수만큼
-	for (int i = 0; i < playerNum; i++) {
-		boolean flag = false;
-		string tempId;
-		do {
-			cout << "아이디를 입력해 주세요: ";
-			cin >> tempId;
-			flag = checkId(tempId);
-		} while (!flag);
-		
-		user[i] = *(new User(tempId));
-	}
-	cout << endl;
-
-	// 공유카드 셋팅
-	dealer->Shard_card();
-	controller->setOpenCard(dealer->getOpenCard());
-	cout << endl;
-
-	// 사용자 카드 셋팅
-	for (int i = 0; i < playerNum; i++) {
-		dealer->Shuffle();
-		user[i].playerDeck();
+class Main {
+public:
+	int main() {
+		playingGame();
 	}
 
-	// 
-}
+	void playingGame() {
+		GameController* controller = new GameController();
+		Dealer* dealer = new Dealer();
+		int playerNum = 3;
+		User user[3];
 
-boolean checkId(string id) {
+		// 플레이어 수 입력
+		playerNum = controller->askPlayerNum();
+		cout << endl;
 
-}
+		// 유저 생성 > 플레이어 수만큼
+		for (int i = 0; i < playerNum; i++) {
+			boolean flag = false;
+			string tempId;
+			do {
+				cout << "아이디를 입력해 주세요: ";
+				cin >> tempId;
+				flag = checkId(tempId);
+			} while (!flag);
+
+			user[i] = *(new User(tempId));
+		}
+		cout << endl;
+
+		// 공유카드 셋팅
+		dealer->Shard_card();
+		controller->setOpenCard(dealer->getOpenCard());
+		cout << endl;
+
+		// 사용자 카드 셋팅
+		for (int i = 0; i < playerNum; i++) {
+			dealer->Shuffle();
+			user[i].playerDeck(*dealer);
+		}
+
+		// 
+	}
+
+	boolean checkId(string id) {
+		ifstream ifs;
+
+	}
+};
